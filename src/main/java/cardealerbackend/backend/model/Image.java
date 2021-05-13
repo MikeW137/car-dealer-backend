13d77f9cef3e3818767b7cf616a5d0b08c162e2d
Michael Wydra <wydrama@gmail.com>
2021-05-13 15:09:17 -0500

    finished image model

--- src/main/java/cardealerbackend/backend/model/Image.java
+++ src/main/java/cardealerbackend/backend/model/Image.java
@@ -1,5 +1,7 @@
 package cardealerbackend.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 
@@ -16,8 +18,47 @@ public class Image {
     private String imageUrl;
 
     @ManyToOne
-    @JoinColumn
+    @JoinColumn(name="Car")
+    @JsonIgnore
     private Inventory individualCar;
 
+    public Image() {
+    }
 
+    public Image(Long id, String imageUrl) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Inventory getIndividualCar() {
+        return individualCar;
+    }
+
+    public void setIndividualCar(Inventory individualCar) {
+        this.individualCar = individualCar;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
 }
