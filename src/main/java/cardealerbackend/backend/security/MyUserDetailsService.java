e37e9db7b077f47077c9a5f1db3032e97d16bcc4
Hristo Papucharov <hpapucharov@gmail.com>
2021-05-14 09:18:09 -0500

    Added security

---
+++ src/main/java/cardealerbackend/backend/security/MyUserDetailsService.java
@@ -0,0 +1,20 @@
+package cardealerbackend.backend.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+}
--- src/main/java/cardealerbackend/backend/service/UserService.java
+++ src/main/java/cardealerbackend/backend/service/UserService.java
@@ -1,4 +1,9 @@
 package cardealerbackend.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
 }
