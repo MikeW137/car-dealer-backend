d1c4dce8477b54aa475e9acfe9d911df9f7afa1f
Hristo Papucharov <hpapucharov@gmail.com>
2021-05-17 09:44:09 -0500

    Testing Maven

---
+++ src/test/java/cardealerbackend/backend/ControllerIntegrationTests.java
@@ -0,0 +1,17 @@
+package cardealerbackend.backend;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerIntegrationTests {
+    //This file tests the primary Book CRUD and all of the GET methods for the tables.
+    @Autowired
+    public MockMvc mockMvc;
+}
